b55c3f4443375dba5ca04508b6abcbf371074349
Jighogho <jessie.ifeanyi@gmail.com>
2022-08-28 21:32:33 +0100

    app update

--- nnsight.py
+++ nnsight.py
@@ -1,12 +1,7 @@
 from flask import Flask
-APP = Flask(__name__)
+app = Flask(__name__)
 
 
-@APP.route('/')
+@app.route('/')
 def hello_world():
     return 'Hello, Welcome to nnsight!\n'
-
-
-
-if __name__ == '__main__':
-    APP.run(host='0.0.0.0', port=8080, debug=True)
